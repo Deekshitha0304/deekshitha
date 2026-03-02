e170ab67d756e410c144a3a936795c515eb3e2da
Deekshitha Chinamuttevi <deekshi.ch97@gmail.com>
2026-03-02 17:39:47 +0530

    drills deletion

--- node/nd-foundations/03-drill.js
+++
@@ -1,7 +0,0 @@
-console.log("Hi");
-
-
-console.log("ARGV:", process.argv);                 // --hello will be added to arguments
-
-
-//created a script in package.json ..so we can just use npm run start as shoertcut
--- node/nd-foundations/hello.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const name = "Deekshi";
-console.log(`Hello, ${name}`);
-//# sourceMappingURL=hello.js.map
